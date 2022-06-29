ecc4d065878b5570b112fadd58f3703b74c80d8f
Eslam Elareef <45187303+Eslam-Elareef@users.noreply.github.com>
2022-06-29 14:21:10 +0200

    update 3

--- src/Components/Home/style.js
+++ src/Components/Home/style.js
@@ -8,7 +8,7 @@ import styled from 'styled-components'
 
 export const HomeSection = styled.div`
     height: 500px;
-    background: url('../images/Home/home-bg.jpg');
+    background-image: url('images/home-bg.jpg');
     background-size: cover;
     background-position: center;
     text-align: center;
